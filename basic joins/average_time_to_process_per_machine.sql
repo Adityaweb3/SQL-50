d169329509d7fa090a079d7b3ddeb976d94d4bd3
adityaweb3 <ap7596112@gmail.com>
2024-01-27 16:31:26 +0530

    1661 average time to process per machine

---
+++ basic joins/average_time_to_process_per_machine.sql
@@ -0,0 +1,9 @@
+-- Problem Link : https://leetcode.com/problems/average-time-of-process-per-machine/
+
+# Write your MySQL query statement below
+select a1.machine_id , Round(AVG(a2.timestamp - a1.timestamp),3) as processing_time  
+FROM Activity a1 INNER JOIN Activity a2 
+ON a1.process_id = a2.process_id 
+AND a1.machine_id = a2.machine_id 
+AND a1.timestamp <a2.timestamp 
+GROUP BY a1.machine_id ;
